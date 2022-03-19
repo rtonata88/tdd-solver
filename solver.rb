668bb2091e4ef70af88ac972f323b18eef491b5c
rtonata88 <rtonata@gmail.com>
2022-03-19 18:18:46 +0200

    Add fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -6,7 +6,19 @@ def factorial(num)
     num * factorial(num - 1)
   end
 
-  def reverse(str)
-    str.reverse
+  def reverse(string)
+    string.reverse
+  end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num
+    end
   end
 end
